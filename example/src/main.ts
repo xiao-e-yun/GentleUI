3ea5e7e6b0a50e72d212cec02987ff35455ce3ab
Xiao_e_yun <67814228+xiao-e-yun@users.noreply.github.com>
2023-01-18 06:59:06 +0000

    add example

---
+++ example/src/main.ts
@@ -0,0 +1,5 @@
+import * as gentleUI from "gentle-ui"
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
---
+++ src/Hello.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+h1.title Hello World
+</template>
+
+<script lang="ts" setup>
+console.log("Hello World")
+</script>
+
+<style lang="scss" module>
+.title {
+    color: #fff;
+    background: #000;
+}
+</style>
