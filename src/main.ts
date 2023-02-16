bd130872529350581f5d539aad10d5177a1e904e
xiao_e_yun <a0970323059@gmail.com>
2023-02-17 07:31:17 +0800

    Fixed example can't build

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,23 @@
-import Code from "./components/Code.vue"
-import Image from "./components/Image.vue"
-import Block from "./components/Block.vue"
-import Button from "./components/Button.vue"
-import Content from "./components/Content.vue"
-import ButtonGroup from "./components/ButtonGroup.vue"
+import Code from "./components/Code.vue";
+import Image from "./components/Image.vue";
+import Block from "./components/Block.vue";
+import Button from "./components/Button.vue";
+import Content from "./components/Content.vue";
+import ButtonGroup from "./components/ButtonGroup.vue";
 
 declare global {
   interface Window {
     GentleUI: {
-      lazyLoader?: IntersectionObserver
-    }
+      lazyLoader?: IntersectionObserver;
+    };
   }
 }
 
-export {
-  Code,
-  Image,
-  Block,
-  Button,
-  Content,
-  ButtonGroup,
-}
+export { Code, Image, Block, Button, Content, ButtonGroup };
 
-/** 
- * Inject variables and functions  
- * 
+/**
+ * Inject variables and functions
+ *
  * Usage in `vite.config.ts`
  * ```typescript
  * export default defineConfig({
@@ -39,23 +32,26 @@ export {
  *   },
  * },
  * ```
-*/
+ */
 
 export function addFunctionsAndVars(
   /**
    * add pack prefix
-   * `vars` -> `[prefix]`  
+   * `vars` -> `[prefix]`
    * `{others}` -> `[prefix]-{others}`
-  */
+   */
   prefix?: string,
   /** add other additionalData */
-  other = "",
+  other = ""
 ) {
-  const vars = prefix ? " as " + prefix : ""
-  const color = prefix ? " as " + prefix + "-color" : ""
-  const fixed = `@use "gentle-ui/_color.scss"${color};@use "gentle-ui/_vars.scss"${vars};` + other;
+  const vars = prefix ? " as " + prefix : "";
+  const color = prefix ? " as " + prefix + "-color" : "";
+  const fixed =
+    `@use "gentle-ui/_color.scss"${color};@use "gentle-ui/_vars.scss"${vars};` +
+    other;
   return (style: string, path: string) => {
-    if (path.includes("GentleUI/dist/assets")) return style
-    return fixed + style
-  }
+    if (style.includes("//GENTLE-UI CORE")) return style;
+    return fixed + style;
+  };
 }
+
