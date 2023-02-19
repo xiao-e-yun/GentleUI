637e324bc2e00b84f5f8fe783f9ef60d320f3927
Xiao_e_yun <67814228+xiao-e-yun@users.noreply.github.com>
2023-02-19 12:42:26 +0800

    Change base url

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -5,6 +5,7 @@ import { addFunctionsAndVars } from "gentle-ui"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: './',
   css: {
     preprocessorOptions: {
       scss: {
